12a9a381b8716f37f447be8a102d5e0ff2ca8c31
Robert Checco <robert.checco@gmail.com>
2019-06-24 17:03:56 -0400

    Insert One into DB

---
+++ mongodb.js
@@ -0,0 +1,29 @@
+/*
+CRUD Operations
+Create, Read, Update, Delete
+*/
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database');
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection('users').insertOne({
+        name: 'Robert',
+        age: 24
+    }, (error, result) => {
+        if (error) {
+            return console.log('Unable to insert user');
+        }
+
+        console.log(result.ops);
+    })
+}); 
