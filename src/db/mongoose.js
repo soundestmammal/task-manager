a497b063a6956cc2b16157d5e10227e0e1703193
Robert Checco <robert.checco@gmail.com>
2019-07-26 12:47:50 -0400

    Create Tasks Model and save to the database

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -21,3 +21,34 @@ me.save().then((me) => {
 }).catch((error) => {
     console.log("Error: ", error);
 });
+
+/* 
+
+Goal: Create a model for tasks
+
+1. Define the model with description and completed fields
+2. Create a new instance of the model
+3. Save the model to the database
+4. Test your work
+
+*/
+
+const Tasks = mongoose.model('Tasks', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+});
+
+const july26 = new Tasks({
+    description: "Go to Flux",
+    completed: false
+});
+
+july26.save().then((july26) => {
+    console.log("This was a success", july26);
+}).catch((error) => {
+    console.log("There was an error, I am so sorry to tell you", error);
+});
