3011abb036554be1decb11a8b65599d680850f54
Robert Checco <robert.checco@gmail.com>
2019-08-15 15:33:21 -0400

    Refactor mongoose.js to put User model in it's own folder, temp

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,82 +1,11 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value){
-            if(value < 0){
-                throw new Error("This was not a valid age");
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("This is not a valid email!");
-            }
-        }
-    },
-    password: {
-        type: String,
-        requried: true,
-        minlength: 7,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(value.toLowercase().includes("password")) {
-                throw new Error("Your password can not contain the word password...");
-            }
-        }
-    }
-});
-
-const me = new User(
-    {
-        name: 'Roberto',
-        age: 56,
-        email: "roberto@email.com",
-        password: "password"
-     })
-
-// me.save().then((me) => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log("Error: ", error);
-// });
-
-/* 
-
-Goal: Create a model for tasks
-
-1. Define the model with description and completed fields
-2. Create a new instance of the model
-3. Save the model to the database
-4. Test your work
-
-Next Challenge:
-
-1. (Description) Eliminate the whitespace and make it required
-2. Make completed optional and set default value to false
-3. Test the work.
-*/
-
-const Tasks = mongoose.model('Tasks', {
+const Task = mongoose.model('Task', {
     description: {
         type: String,
         required: true,
@@ -86,15 +15,4 @@ const Tasks = mongoose.model('Tasks', {
         type: Boolean,
         default: false
     }
-});
-
-const july26 = new Tasks({
-    description: "Go to Flux",
-    completed: false
-});
-
-july26.save().then((july26) => {
-    console.log("This was a success", july26);
-}).catch((error) => {
-    console.log("There was an error, I am so sorry to tell you", error);
 });
