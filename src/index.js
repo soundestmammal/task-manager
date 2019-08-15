0d54173e160435af04f8f5c1caf06d665df71480
Robert Checco <robert.checco@gmail.com>
2019-08-15 18:46:52 -0400

    Import Task model into index.js, also create post endpoint for new task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('./db/mongoose');
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,9 +14,19 @@ app.post('/users', (req, res) => {
     user.save().then(() => {
         res.send(user);
     }).catch((e) => {
-        console.log("There is an error!", e);
+        res.status(400).send(e);
     });
-})
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.send(task);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
 
 app.listen(PORT, () => {
     console.log("Server is up on the port");
