d4cb6f8cb0fe83d16504312c722b2b991412e9b8
Robert Checco <robert.checco@gmail.com>
2019-08-16 13:35:27 -0400

    Practice creating promises, resolve, reject, nested promise -> chaining

--- playground/8-promises.js
+++ playground/8-promises.js
@@ -1,15 +1,59 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        // resolve([1,2,3]);
-        reject('This was an error');
-    }, 2000)
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         // resolve([1,2,3]);
+//         reject('This was an error');
+//     }, 2000)
+// })
+
+// doWorkPromise.then((result) => {
+//     console.log('Success', result);
+// }).catch((error) => {
+//     console.log('Error!', error);
+// })
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a+b);
+        }, 2000)
+    })
+}
+
+
+add(1, 2).then((sum) => {
+    console.log(sum);
+    add(sum, 5).then((sum2) => {
+        console.log(sum2);
+    }).catch((e) => {
+        console.log(e);
+    })
+}).catch((e) => {
+    console.log(e);
 })
 
-doWorkPromise.then((result) => {
-    console.log('Success', result);
-}).catch((error) => {
-    console.log('Error!', error);
+// Recreate with promise chaining...
+
+/* Main point is
+
+1. returning a promise
+2. chaining the .then()
+
+*/
+add(1, 1).then((sum) => {
+    console.log(sum);
+    return add(sum, 4);
+}).then((sum2) => {
+    console.log(sum2);
+}).catch((e) => {
+    console.log(e);
 })
+
+
+
+
+
+
+
 
 //
 //                                fulfilled
