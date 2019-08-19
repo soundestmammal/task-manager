228744fe78c27e6cf29d1b913c2deb04efe69610
Robert Checco <robert.checco@gmail.com>
2019-08-19 17:51:59 -0400

    Delete a User by ID

--- src/index.js
+++ src/index.js
@@ -124,7 +124,21 @@ app.patch('/users/:id', async (req, res) => {
     } catch(e) {
         res.status(400).send();
     }
-})
+});
+
+app.delete('/users/:id', async(req, res) => {
+    const _id = req.params.id;
+    
+    try {
+        const user = await User.findByIdAndDelete(_id);
+        if(!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch(e) {
+        res.status(400).send();
+    }
+});
 
 app.listen(PORT, () => {
     console.log("Server is up on the port");
