51ccfa6ff80cf43075120ce4c28e63239c9f6ecc
Robert Checco <robert.checco@gmail.com>
2019-08-20 14:58:21 -0400

    Change the Task model to support future middleware

--- src/models/task.js
+++ src/models/task.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
+const taskSchema = new mongoose.Schema(
+    {
+        description: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
     }
-});
+)
+
+const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
