1ebd682abc85e2cd2ce6b0b08d7e12f8d8f6237c
Robert Checco <robert.checco@gmail.com>
2019-08-22 15:01:30 -0400

    Call generateAuthToken from within the route

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const router = new express.Router();
@@ -10,7 +11,8 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken();
+        res.status(201).send({ user, token });
     } catch(e) {
         res.status(400).send(e); 
     }
@@ -19,7 +21,9 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
-        res.send(user);
+        const token = await user.generateAuthToken();
+
+        res.send({ user, token });
     } catch(e) {
         res.status(400).send();
     }
