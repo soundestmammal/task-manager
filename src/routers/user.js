a05f20cf91a477901198f77f53618c4d1de399ce
Robert Checco <robert.checco@gmail.com>
2019-08-24 18:11:46 -0400

    Logout Route, post request

--- src/routers/user.js
+++ src/routers/user.js
@@ -28,7 +28,20 @@ router.post('/users/login', async (req, res) => {
     } catch(e) {
         res.status(400).send();
     }
-})
+});
+
+router.post('/users/logout', auth, async(req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+        await req.user.save();
+
+        res.send();
+    } catch(e) {
+        res.status(500).send();
+    }
+});
 
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user);
