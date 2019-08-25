6dd2023513e95f6a99deb97a6d1d626bbd71ea7b
Robert Checco <robert.checco@gmail.com>
2019-08-24 22:54:05 -0400

    Add an owner data to the tasks model

--- src/models/task.js
+++ src/models/task.js
@@ -11,6 +11,10 @@ const taskSchema = new mongoose.Schema(
         completed: {
             type: Boolean,
             default: false
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
         }
     }
 )
@@ -18,3 +22,13 @@ const taskSchema = new mongoose.Schema(
 const Task = mongoose.model('Task', taskSchema);
 
 module.exports = Task;
+
+/*
+There are two ways, to do this...
+
+We can either have the user store the ids of the tasks, 
+
+Or 
+
+We can store the id of the user on the task data field
+*/
