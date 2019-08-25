d5f5a13e4a2d1fbd8f0f944820d46dbbd626d60e
Robert Checco <robert.checco@gmail.com>
2019-08-25 12:54:06 -0400

    Clean up index.js to be more readable

--- src/index.js
+++ src/index.js
@@ -8,21 +8,6 @@ const taskRouter = require('./routers/task');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if(req.method === "GET") {
-//         res.send("Get requests are disabled.");
-//     } else {
-//         next();
-//     }
-
-//      // Without this the request would never end...
-//     /* If we never call next, then the route handler will never run.*/
-// });
-
-// app.use((req, res, next) => {
-//         res.status(503).send("The site is under maintenance give us some time...");
-// })
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -30,48 +15,3 @@ app.use(taskRouter);
 app.listen(PORT, () => {
     console.log("Server is up on port", PORT);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-    // const task = await Task.findById("5d61fa2cbcc5a80d6af21f08");
-    // await task.populate('owner').execPopulate();
-    // console.log(task.owner);
-
-    const user = await User.findById("5d61f9859426ba0d44b0e5e0");
-    await user.populate('tasks').execPopulate();
-    console.log(user.tasks);
-
-    // const user = await User.findById(task.owner);
-    // console.log(user.name);
-};
-
-main();
-
-
-
-
-
-
-// const jwt = require('jsonwebtoken');
-
-// const myFunction = () => {
-//     const token = jwt.sign({ _id: 'abc123' }, "hereismycryptokey", { expiresIn: '5 seconds'});
-//     console.log(token);
-//     const data = jwt.verify(token, 'hereismycryptokey');
-//     console.log(data);
-// }
-
-// myFunction();
-
-// const pet = {
-//     name: 'Luna'
-// }
-
-// pet.toJSON = function() {
-//     console.log(this);
-//     return {};
-// }
-
-// console.log(JSON.stringify(pet)); 
