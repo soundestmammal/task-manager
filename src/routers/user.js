28245f4b69b99dfe67bcbc229376be320deed0c2
Robert Checco <robert.checco@gmail.com>
2019-08-31 16:46:11 -0400

    Import and use the sendWelcomeEmail function upon signing up

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
+const { sendWelcomeEmail } = require('../emails/account');
 
 const upload = multer({
     limits: {
@@ -26,6 +27,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save();
+        sendWelcomeEmail(user.email, user.name);
         const token = await user.generateAuthToken();
         res.status(201).send({ user, token });
     } catch(e) {
