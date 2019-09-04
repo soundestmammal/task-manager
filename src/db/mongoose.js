a7f163f6d73f59d735203284e2586189a3bfabf6
Robert Checco <robert.checco@gmail.com>
2019-09-03 21:58:05 -0400

    Assign env variable to a js const

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
+const MONGODB_URL = process.env.MONGODB_URL;
+
+mongoose.connect(MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
